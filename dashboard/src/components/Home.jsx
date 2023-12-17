781c73dbc3459be9f01ec5fdc341510a893a5793
Ognen Veljanovski <127666100+ognen007@users.noreply.github.com>
2023-12-17 16:10:52 +0100

    Undefined objects error
    fixed

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -17,41 +17,50 @@ const Home = () => {
     fetchData();
   }, []);
 
-  return (
-    <div>
-      <h1>Data</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Draw Parcel</th>
-            <th>Form Data</th>
-          </tr>
-        </thead>
-        <tbody>
-  {data.map((item, index) => (
-    <tr key={index}>
-      <td>{item.drawParcel}</td>
-      <td>
-        {Object.entries(item.formData).map(([key, value]) => (
-          <p key={key}>
-            {`${key}: ${value instanceof Object ? value.name : value}`}
-          </p>
-        ))}
-      </td>
-    </tr>
-  ))}
-</tbody>
+  const handleLogout = () => {
+    setLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+  };
 
-      </table>
+  return (
+<div>
+  <table className="data-table">
+    <thead>
+      <tr>
+        <th>Draw Parcel</th>
+        <th>Form Data</th>
+      </tr>
+    </thead>
+    <tbody>
+    {data.map((item, index) => (
+  <tr key={index}>
+    <td>{item.drawParcel}</td>
+    <td>
+      {item.formData && Object.entries(item.formData).map(([key, value]) => {
+        if (value instanceof Object) {
+          return Object.entries(value).map(([subKey, subValue]) => (
+            <p key={`${key}-${subKey}`}>
+              {`${subKey}: ${subValue}`}
+            </p>
+          ));
+        } else {
+          return (
+            <p key={key}>
+              {`${key}: ${value}`}
+            </p>
+          );
+        }
+      })}
+    </td>
+  </tr>
+))}
+
+    </tbody>
+  </table>
+  <button className="logout-button" onClick={handleLogout}>Logout</button>
     </div>
   );
 };
 
 export default Home;
 
-const handleLogout = () => {
-  setLoggedIn(false);
-  localStorage.removeItem("isLoggedIn");
-};
-
-<button onClick={handleLogout}>Logout</button>;
